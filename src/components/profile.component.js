c8ceef55b2270372d9d295f5705cca1fffe0737f
srikar-1997 <thalladasrikar7@msitprogram.net>
2020-07-04 06:18:03 +0530

    removed image exits

--- src/components/profile.component.js
+++ src/components/profile.component.js
@@ -16,15 +16,6 @@ import { Link } from 'react-router-dom'
 import Edit from "./editprofile.component"
 
 
-// The "callback" argument is called with either true or false
-// depending on whether the image at "url" exists or not.
-function imageExists(url, callback) {
-      var img = new Image();
-      img.onload = function() { callback(true); };
-      img.onerror = function() { callback(false); };
-      img.src = url;
-}
-
 
 function Session(props){
   console.log("status",props.this.status)
